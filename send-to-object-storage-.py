eb68e0e7bb8c3cdd7d482e66a39048c41c555aaf
Roya <164702751+Roya-2024@users.noreply.github.com>
2026-06-23 14:28:47 -0700

    Add AWS upload script

---
+++ send-to-object-storage-.py
@@ -0,0 +1,19 @@
+import os
+import boto3
+
+bucket_name = "roya-data-eng-20250610"
+directory = "./stage"
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION"),
+)
+
+for filename in os.listdir(directory):
+    file_path = os.path.join(directory, filename)
+
+    if os.path.isfile(file_path):
+        s3.upload_file(file_path, bucket_name, filename)
+        print(f"{filename} uploaded successfully")
---
+++ send-to-object-storage.py
@@ -0,0 +1,19 @@
+import os
+import boto3
+
+bucket_name = "roya-github-actions-bucket"
+directory = "./stage"
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION", "eu-central-1"),
+)
+
+for filename in os.listdir(directory):
+    file_path = os.path.join(directory, filename)
+
+    if os.path.isfile(file_path):
+        s3.upload_file(file_path, bucket_name, filename)
+        print(f"{filename} uploaded successfully")
